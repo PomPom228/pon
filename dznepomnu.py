81f3247f6d6e96655918b8f395be0b98ff444f3c
Pon <irinakardinal001@gmail.com>
2023-06-09 18:38:54 +0300

    19 05 commit

---
+++ dznepomnu.py
@@ -0,0 +1,19 @@
+'''class Human:
+    height = 170
+    satiety = 50
+
+class Student(Human):
+    satiety = 0
+
+class Worker(Human):
+    satiety = 100
+
+
+nick=Student()
+ann=Worker()
+print(nick.height)
+print(nick.satiety)
+print(ann.height)
+print(ann.satiety)
+'''
+
